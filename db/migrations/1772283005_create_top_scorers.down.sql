2b09cb9e00c9051a570a8010d5ae4499e4bac3f1
rifky <rifky.rachman@gmail.com>
2026-02-28 21:57:37 +0700

    remove docker-compose.yml from tracking

---
+++ db/migrations/1772283005_create_top_scorers.down.sql
@@ -0,0 +1,32 @@
+CREATE TABLE IF NOT EXISTS top_scorers (
+    id BIGSERIAL PRIMARY KEY,
+    type_id BIGINT NOT NULL,
+    type_name VARCHAR(100) NOT NULL,
+
+    rank INTEGER NOT NULL,
+    total INTEGER NOT NULL,
+
+    player_id BIGINT NOT NULL,
+    season_id BIGINT NOT NULL,
+    season_name VARCHAR(50) NOT NULL,
+
+    participant_id BIGINT NOT NULL,
+
+    player_name VARCHAR(150) NOT NULL,
+    image_player TEXT,
+
+    nationality VARCHAR(100),
+    image_nationality TEXT,
+
+    participant_name VARCHAR(150),
+    image_participant TEXT,
+
+    position_name VARCHAR(100),
+
+    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    deleted_at TIMESTAMP NULL
+);
+
+CREATE UNIQUE INDEX IF NOT EXISTS uq_top_scorers_identity
+    ON top_scorers (season_id, player_id, type_id);
